ab554e78477247ccb87d73529ad7e646449e9af8
wist18 <razvan.wist@gmail.com>
2024-05-24 18:59:00 +0200

    add automated script(run_pass.sh) for pass-run task

---
+++ run_pass.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+run_task() {
+  task run-pass
+  return $?
+}
+
+while true; do
+  run_task
+  status=$?
+  if [ $status -eq 0 ]; then
+    echo "All files have been processed successfully."
+    break
+  elif [ $status -eq 137 ]; then
+    echo "Process was killed due to OOM. Exiting."
+    exit 137
+  else
+    echo "Some files are still unprocessed. Rerunning the task..."
+    sleep 0.3  # Delay before rerunning
+  fi
+done
